ea1608fb0a7e13e1fc9bae8c39fe33faec8a652f
guor <guor0707@qq.com>
2019-07-17 17:43:11 +0800

    TypeScript 学习笔记

---
+++ typescript/基础类型.ts
@@ -0,0 +1,48 @@
+/**
+ * Boolean
+ */
+let isDone: boolean = false;
+
+
+
+/**
+ * Number
+ */
+let decLiteral: number = 6;
+
+
+
+/**
+ * String
+ */
+let myName: string = 'Tom';
+
+
+
+/**
+ * void 表示没有任何返回值的函数, 只能将它赋值为 undefined 和 null
+ */
+function alertName(): void {
+  alert('My name is Tom');
+}
+let unusable: void = undefined;
+
+
+
+/**
+ * undefined 只能被赋值为 undefined
+ * null 只能被赋值为 null
+ */
+let u: undefined = undefined;
+let n: null = null;
+
+
+
+/**
+ * Any 允许被赋值为任意类型。
+ * 在任意值上访问任何属性和方法都是允许的
+ */
+let myFavoriteNumber: any = 'seven';
+myFavoriteNumber = 7;
+let anyThing: any = 'Tom';
+anyThing.setName('Jerry');
