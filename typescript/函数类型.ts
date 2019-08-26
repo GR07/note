764e45d034f2bf7f61fd45a68c32b844b48d65b5
guor <guor0707@qq.com>
2019-08-26 17:26:57 +0800

    笔记更新

--- typescript/函数类型.ts
+++ typescript/函数类型.ts
@@ -1,11 +1,11 @@
 /**
- * 对函数的输入和输出进行约束
+ * 对函数的输入（传值）和输出（返回值）进行约束
  */
 
 
 
 /**
- * 函数声明
+ * ts中的 函数声明
  */
 function sum(x: number, y: number): number {
   return x + y;
@@ -14,13 +14,52 @@ sum(1, 2); // 参数的数量也必须一致
 
 
 
+
 /**
- * 函数表达式：分为左边约束、右边约束，两个部分。
+ * ts中的 函数表达式：分为左边约束、右边约束，两个部分。
  */
 // mySum: (x: number, y: number) => number 表示函数类型并约束输入参数的类型，和输出类型
 let mySum: (x: number, y: number) => number = function (x: number, y: number): number {
   return x + y;
 };
+
+
+
+
+/**
+ * 没有返回值：void
+ */
+function ab (): void {
+  console.log(123)
+}
+
+
+
+
+/**
+ * 可选参数： 与接口的可选属性类似，但位置必须是最后一个参数
+ */
+// 参数 b 可传可不传
+function buildName(a: string, b?: string) {
+  if (b) {
+    return a + ' ' + b;
+  } else {
+    return a;
+  }
+}
+
+
+
+
+/**
+ * 参数默认值： es5没有  es6和ts都可以设置。
+ * 会将添加了默认值的参数识别为可选参数
+ */
+function defaultName(a: string, b: string = 'Cat') {
+  return a + ' ' + b;
+}
+let jimcat = defaultName('Jim', 'Cat');
+let jim = defaultName('Jim');
 
 
 
@@ -37,31 +76,6 @@ mySearch = function(a: string, b: string) {
 
 
 
-/**
- * 可选参数： 与接口的可选属性类似，但位置必须是最后一个参数
- */
-function buildName(a: string, b?: string) {
-  if (b) {
-    return a + ' ' + b;
-  } else {
-    return a;
-  }
-}
-let tomcat = buildName('Tom', 'Cat');
-let tom = buildName('Tom');
-
-
-
-/**
- * 参数默认值： TypeScript 会将添加了默认值的参数识别为可选参数
- */
-function defaultName(a: string, b: string = 'Cat') {
-  return a + ' ' + b;
-}
-let jimcat = defaultName('Jim', 'Cat');
-let jim = defaultName('Jim');
-
-
 
 /**
  * 剩余参数： ...rest 获取函数中的剩余参数
@@ -74,6 +88,7 @@ function push(array: any[], ...items: any[]) {
 }
 let a = [];
 push(a, 1, 2, 3);
+
 
 
 
