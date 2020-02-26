d2c935a02fa38dba8a29f8e40403037ff1688993
guor <guor0707@qq.com>
2020-02-26 15:15:42 +0800

    笔记更新

---
+++ vue/$nextTick.js
@@ -0,0 +1,27 @@
+/**
+ * 简单来说，Vue 在修改数据后，视图不会立刻更新，而是等同一事件循环中的所有数据变化完成之后，再统一进行视图更新。
+ */
+
+
+//改变数据
+vm.message = 'changed'
+
+//想要立即使用更新后的DOM。这样不行，因为设置message后DOM还没有更新
+console.log(vm.$el.textContent) // 并不会得到'changed'
+
+//这样可以，nextTick里面的代码会在DOM更新后执行
+Vue.nextTick(function(){
+  console.log(vm.$el.textContent) //可以得到'changed'
+})
+
+
+
+
+// 应用场景：需要在视图更新之后，基于新的视图进行操作。
+
+
+
+// 需要注意的是，在 created 和 mounted 阶段，如果需要操作渲染后的试图，也要使用 nextTick 方法。
+
+
+// 注意 mounted 不会承诺所有的子组件也都一起被挂载。如果你希望等到整个视图都渲染完毕，可以用 vm.$nextTick 替换掉 mounted
