883ec5aec25c2c4a1c5373a4dd0efa9e21c0075c
guor <guor0707@qq.com>
2020-03-01 17:06:26 +0800

    笔记更新

--- vue/router-view复用问题.js
+++ vue/router-view复用问题.js
@@ -1,10 +1,15 @@
 /**
  * 真是业务场景中, 可能两个页面使用同一个组件, 默认情况下两个页面切换时不会触发created或者mounted钩子,
- * 官方说可以用 watch 监听 $route 变化来处理,
+ * 
+ * 
+ * 方法一: 官方说可以用 watch 监听 $route 变化来处理.
+ * 
+ * 方法二: 组件路由更新时(组件被复用时)调用 beforeRouteUpdate (to, from, next) {}
  * 
  * 还有一种方便的办法: 在 router-view 上加上一个唯一的key, 来保证路由切换时都会重新渲染触发钩子了.
  */
 
+// key
 <router-view :key="key"></router-view>
 
 computed: {
