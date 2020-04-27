7810ad245e0982920d0f8436d9156d1cd4d1c478
maxingcong <1211285283@qq.com>
2020-04-27 22:00:17 +0800

    笔记更新

--- javascript/数组操作整理.js
+++ javascript/数组操作整理.js
@@ -161,3 +161,14 @@ const states = ["Alabama", "Alaska"],
 this.list = this.states.map(item => {
   return { value: `value:${item}`, label: `label:${item}` };
 });
+
+
+
+// 过滤出符合条件的项
+const restaurants = [
+  { "value": "三全鲜食（北新泾店）", "address": "长宁区新渔路144号" },
+  { "value": "Hot honey 首尔炸鸡（仙霞路）", "address": "上海市长宁区淞虹路661号" },
+  { "value": "新旺角茶餐厅", "address": "上海市普陀区真北路988号创邑金沙谷6号楼113" }]
+var results = queryString ? restaurants.filter((restaurant) => {
+  return (restaurant.value.toLowerCase().includes(queryString.toLowerCase()))
+}) : restaurants;
