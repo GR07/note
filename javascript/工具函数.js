d378821722fcdf440deb2cd2dec0b0961a759c6c
guor <guor0707@qq.com>
2020-12-29 20:38:17 +0800

    保留小数工具函数

--- javascript/工具函数.js
+++ javascript/工具函数.js
@@ -18,32 +18,34 @@ function isUndefined (obj) {
 	return obj === void 0;
 }
 
-// 金额转大写
-function digitUppercase(n) {
-  if (n == 0) {
-      return "零";
+/**
+ * 保留几位小数
+ * @param {String} value 当前输入
+ * @param {Number} how 保留几位
+ * @returns {String} 替换后的值
+ */
+export function decimals(value, how) {
+  // 必须数字
+  let mustNum = /[^\d.]/g;
+  // 小数前几位
+  let beforeFloat = /^(\d{4,})[^.]$/g;
+  // 必有小数点
+  let mustFloat = /^\./g;
+  // 只有一个小数点
+  let onlyFloat = /\.{2,}/g;
+  // 两位小数
+  let twoNum = /^(\d+)\.(\d{1,2}).*$/g;
+  // 四位小数
+  let fourNum = /^(\d+)\.(\d{1,4}).*$/g;
+
+  if (how === 2) {
+      return value.replace(mustNum,"").replace(beforeFloat,"$1").replace(mustFloat,"").replace(onlyFloat,".").replace(twoNum,"$1.$2");
   }
-  if (!/^(\+|-)?(0|[1-9]\d*)(\.\d+)?$/.test(n))
-      return "数据非法";
-  var unit = "仟佰拾亿仟佰拾万仟佰拾元角分", str = "";
-  n += "00";
-  var a = parseFloat(n);
-  if (a < 0) {
-      n = n.substr(1);
-  }
-  var p = n.indexOf('.');
-  if (p >= 0) {
-      n = n.substring(0, p) + n.substr(p + 1, 2);
-  }
-  unit = unit.substr(unit.length - n.length);
-  for (var i = 0; i < n.length; i++)
-      str += '零壹贰叁肆伍陆柒捌玖'.charAt(n.charAt(i)) + unit.charAt(i);
-  if (a > 0) {
-      return str.replace(/零(仟|佰|拾|角)/g, "零").replace(/(零)+/g, "零").replace(/零(万|亿|元)/g, "$1").replace(/(亿)万|壹(拾)/g, "$1$2").replace(/^元零?|零分/g, "").replace(/元$/g, "元整");
-  } else {
-      return "负" + str.replace(/零(仟|佰|拾|角)/g, "零").replace(/(零)+/g, "零").replace(/零(万|亿|元)/g, "$1").replace(/(亿)万|壹(拾)/g, "$1$2").replace(/^元零?|零分/g, "").replace(/元$/g, "元整");
+  if (how === 4) {
+      return value.replace(mustNum,"").replace(mustFloat,"").replace(onlyFloat,".").replace(fourNum,"$1.$2");
   }
 }
+
 
 // 百度经纬度转高德
 function bMapTransMap(lng, lat) {
