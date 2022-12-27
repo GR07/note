2e4b4329140a095f53d5f1fcbd1721705ac3defc
guor <guor0707@qq.com>
2022-12-28 00:28:21 +0800

    设计模式学习-工厂模式

--- run.js
+++ run.js
@@ -25,3 +25,30 @@ let a = Alert('a')
 console.log(a);
 let b = Alert('b')
 console.log(b);
+
+
+class Product1 {
+  product() {
+  console.log("生产一线");
+  }
+  }
+  class Product2 {
+  product() {
+  console.log("生产二线");
+  }
+  }
+  class Factory {
+  constructor() {
+  this.Product1 = Product1;
+  this.Product2 = Product2;
+  }
+  create(name, callBack) {
+  const product = new this[name]();
+  product.product();
+  return callBack("susess");
+  }
+  }
+  let p = new Factory();
+  p.create("Product1", (res) => {
+  console.log(res);
+  });
